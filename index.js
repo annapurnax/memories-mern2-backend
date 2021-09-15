fdcf39c1de04c2e9c335ecfdcf33bf4f19f18750
annu <74466018+annapurnax@users.noreply.github.com>
2021-09-16 02:42:19 +0530

    change

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://dbuser:admin@cluster0.uzsiu.mongodb.net/memories?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
