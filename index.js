5c56346d07d0bc14d14ba7ce62bd297a62189aa3
annu <74466018+annapurnax@users.noreply.github.com>
2021-09-16 01:42:37 +0530

    changes

--- index.js
+++ index.js
@@ -11,33 +11,30 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postroutes from "./routes/posts.js";
-import dotenv from "dotenv";
 const app = express(); //we use the express methods on tht app instance
 
 app.use(bodyParser.json({ limit: "30mb", extended: "true" })); //images will be sent with a certain limit
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" })); //setting up the bodyParser
 app.use(cors()); //shd be before app.use()
 app.use(express.json());
-dotenv.config();
 
 app.use("/posts", postroutes); //every route inside postroutes starts with /posts
-const port = process.env.PORT || 4000; //PORT is an environment variable
-
+const PORT = process.env.PORT || 4000; //PORT is an environment variable
+const CONNECTION_URL =
+  "mongodb+srv://dbuser:admin@cluster0.uzsiu.mongodb.net/memories?retryWrites=true&w=majority";
 mongoose
-  .connect(process.env.CONNECTION_URL, {
+  .connect(CONNECTION_URL, {
     //to prevent errors in the console
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
   })
   .then(() =>
-    app.listen(port, () => {
-      console.log("Server running on port");
-    })
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
   )
-  .catch((err) => {
-    console.log(err);
-  });
+  .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set("useFindAndModify", false);
 const connection = mongoose.connection;
