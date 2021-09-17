fbacbc4b2e2ce84f8284b6aa467240400d5755ae
annu <74466018+annapurnax@users.noreply.github.com>
2021-09-17 19:42:14 +0530

    changes

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-
+app.use(express.json());
 app.use("/posts", postRoutes);
 app.get((req, res) => {
   res.send("Yooo");
